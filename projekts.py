908b6d5084dc3a2982157a219485d445b684c41c
Krisjanis Vitins <144622271+krisjanisvitins@users.noreply.github.com>
2024-01-18 14:31:51 +0000

    2

--- projekts.py
+++ projekts.py
@@ -4,20 +4,33 @@ fails='Tests.xlsx'
 izvele=input("Vai jūs vēlaties lasīt vai pievienot izmaksas? (Lasīt 'L'/ Pievienot 'P')").upper()
 
 if izvele == 'L':
-    df=pandas.read_excel(fails)
-    print(df)
+    izvele_lasit=input("Ko jūs vēlaties lasīt? (Visu tabulu 'T'/ Izmaksas noteiktā mēnesī 'M')").upper()
+    if izvele_lasit == 'T':
+        df=pandas.read_excel(fails)
+        print(df)
+    elif izvele_lasit == 'M':
+        izvele_menesis=input("Kura mēneša izmaksas jūs vēlaties redzēt? (01-12))")
+        df=pandas.read_excel(fails, parse_dates=['Datums'])
+
+        if df[df['Datums'].dt.month == int(izvele_menesis)]:
+            izdevumi = df[df['Datums'].dt.month == int(izvele_menesis)]['Summa'].sum()
+            print(izdevumi)
+
+        
+
+   
 
 elif izvele == 'P':
     df=pandas.read_excel(fails)
 
-    datums = input("Ievadiet datumu (DD-MM-YYYY): ")
+    datums = input("Ievadiet datumu (YYYY-MM-DD): ")
     kategorija = input("Ievadiet izdevuma kategoriju: ")
     apraksts = input("Ievadiet izdevuma aprakstu: ")
     summa = input("Ievadiet izdevuma summu: ")
 
     kolonnas = {'Datums': datums, 'Kategorija': kategorija, 'Apraksts': apraksts, 'Summa': summa}
-    df = pandas.concat([df, pandas.DataFrame([kolonnas])])
-    df.to_excel(fails)
+    df = pandas.concat([df, pandas.DataFrame([kolonnas])], ignore_index=True)
+    df.to_excel(fails, index=False)
 
 else:
     print("Jāizvēlas 'L' vai 'P'!")
